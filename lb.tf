7e22859ea408491d6cf7b4af087c789cb7953723
Lord Clinton Devops/Cloud Engineer <139438345+Lordclintondevops@users.noreply.github.com>
2024-05-26 15:03:31 -0700

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -3,7 +3,7 @@
 resource "aws_lb" "lordlb" {
   name            = "lordlb"
   security_groups = [aws_security_group.lordsg.id]
-  subnets         = [aws_subnet.lordsub1.id, aws_subnet.lordsub2.id, aws_subnet.lord3.id]
+  subnets         = [aws_subnet.lordsub1.id, aws_subnet.lordsub2.id, aws_subnet.lordsub3.id]
 
   tags = {
     Name = "lordlb"
